38567aef29bc2db67178affd7b4e2aa605c4495c
KeenanBILL <t19008@tsiba.ac.za>
2023-02-25 15:57:17 +0200

    Frontend

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,34 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
+const URL = 'https://awesomebed-tr4f.onrender.com/';
+
 
 export default createStore({
   state: {
+    users: null,
+    user: null,
+    products: null,
+    product: null,
+    showSpinner: true,
+    message: null
   },
   getters: {
   },
   mutations: {
+    setUsers(state,values) {
+      state.users = values  
+    }
   },
   actions: {
+    async fetchUsers(context){
+      const res = await axios.get(`${URL}users`)
+      const {results, err} = await res.data;
+      if(results) {
+        context.commit('setUsers', results)
+      } else{
+        context.commit('setMessage', err)
+      }
+    }
   },
   modules: {
   }
---
+++ src/views/AdminView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <div class="row">
+        <h2>Container</h2>
+    </div>
+    <div class="row">
+        <table>
+          <thead>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>User Profile</th>
+          </thead>
+        </table>
+    </div>
+  </div>
+</template>
+
+<script>
+import 
+
+export default{
+
+}
+</script>
