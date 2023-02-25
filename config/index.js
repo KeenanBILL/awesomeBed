f9751f8e7d6294ee9739c776fa4251fbf6531526
KeenanBILL <t19008@tsiba.ac.za>
2023-02-25 09:51:13 +0200

    Folder Structure

---
+++ config/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+let {createPool} = require('mysql');
+
+let connection = createPool({
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+    multipleStatements: true
+})
+
+module.exports = connection
